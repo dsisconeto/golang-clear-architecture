5ad79d22f491a96512e2836967d03ec5e6373b09
Dejair Sisconeto <dejairsisconeto23@gmail.com>
2018-10-06 03:23:03 -0300

    adicionando DotEnv

--- api/main.go
+++ api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/dsisconeto/arquitetura/api/handler"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 )
@@ -9,8 +11,9 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	handler.ProductRegisterRouters(mux)
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
-
-
-
